759b6a1fb80fc5a685b812f97600430ad544b324
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-03-10 00:34:23 +0100

    Add some type signatures

--- src-lib/Parametric/Parser.hs
+++ src-lib/Parametric/Parser.hs
@@ -36,18 +36,24 @@ abs cons lambda ident colon qualifier dot body
   = cons <$> try (lambda *> ident <* colon) <*> qualifier <*> (dot *> body)
 
 -- left-recursive syntax pattern: "x -> y"
+arr :: Applicative f => (a1 -> a2 -> r) -> f ignored -> f a2 -> f (a1 -> r)
 arr cons arrow simple = flip cons <$> (arrow *> simple)
 
 -- left-recursive syntax pattern: "x y"
+app :: Functor f => (a1 -> a2 -> r) -> f a2 -> f (a1 -> r)
 app cons simple = flip cons <$> simple
 
 -- non-recursive syntax pattern: "ident"
+var :: Functor f => (a -> b) -> f a -> f b
 var cons ident = cons <$> ident
 
 -- non-recursive syntax pattern: "constant"
+con :: Functor f => a -> f b -> f a
 con cons constant = cons <$ constant
 
+withPos :: (Position -> a -> b) -> GenParser tok st a -> GenParser tok st b
 withPos f p = setPos f <$> getPosition <*> p <*> getPosition
 
+setPos :: (Position -> a -> r) -> SourcePos -> a -> SourcePos -> r
 setPos f p1 x p2 = f (Position (sourceName p1) (sourceLine p1) (sourceLine p2) (sourceColumn p1) (pred $ sourceColumn p2)) x
 
